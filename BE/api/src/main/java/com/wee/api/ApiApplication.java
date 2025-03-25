36779862abd7440dc94a71ed54b43c3d734531bf
yeen28 <yestar28@gmail.com>
2025-03-25 17:32:52 +0900

    bug/BE/다른 모듈의 api를 호출할 수 없음 [Task] - @SpringBootApplication을 가지고 있는 Application에 호출하고 싶은 controller 경로를 추가해서 빈으로 스캔될 수 있도록 해야 합니다.

--- BE/api/src/main/java/com/wee/api/ApiApplication.java
+++ BE/api/src/main/java/com/wee/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.wee.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.wee.api", "com.wee.dm"}) // dm 모듈의 패키지를 추가
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
